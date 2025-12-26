ed48f556d91cc6a99b68346a5006105f811919f6
thepriyanshujangid <priyanshujangid@yahoo.com>
2025-12-26 15:23:56 +0530

    redwood-kernel: Update to Scarlet-v4.0-beta1

--- kernel-headers/asm/unistd_32.h
+++ kernel-headers/asm/unistd_32.h
@@ -426,6 +426,8 @@
 #define __NR_fspick 433
 #define __NR_pidfd_open 434
 #define __NR_clone3 435
+#define __NR_close_range 436
 #define __NR_process_madvise 440
+#define __NR_epoll_pwait2 441
 
 #endif /* _ASM_X86_UNISTD_32_H */
